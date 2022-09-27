62d1adda6050b6c3fae1ef74e74d5844e52973bc
Bruno <bruno@medeo-health.com>
2022-09-27 11:26:55 +0200

    add prettier

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <Header onToggle={() => ({})} theme="light"/>
+      <Header onToggle={() => ({})} theme="light" />
     </div>
   );
 }
