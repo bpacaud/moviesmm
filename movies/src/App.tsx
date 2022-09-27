ff5f73b1123c7c151ee13fb2a54d734a93d65cbe
Bruno <bruno@medeo-health.com>
2022-09-27 11:05:05 +0200

    add eslint

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Header onToggle={() => {}} theme="light"/>
+      <Header onToggle={() => ({})} theme="light"/>
     </div>
   );
 }
