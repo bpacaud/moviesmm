f3ebe9f786943de5618581a7d3c1165bdd0cc680
Bruno <bruno@medeo-health.com>
2022-09-28 16:36:34 +0200

    add tests for rendering of landing page

--- movies/src/App.test.tsx
+++ movies/src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders elements of landing page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Movies/i);
+  const search = screen.getByPlaceholderText(/Rechercher un film.../i);
+  expect(title).toBeInTheDocument();
+  expect(search).toBeInTheDocument();
 });
